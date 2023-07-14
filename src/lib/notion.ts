abb4d9cd81c0de5377eccf42c0136ee7dbe59009
simple1017 <simple01281017@gmail.com>
2023-07-14 19:00:36 +0800

    feat: render the page content blocks to markdown text with page id

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -1,3 +1,12 @@
 import { Client } from "@notionhq/client";
 
 export const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+export const CaseStudyPosts = {
+  'hachipay': {
+    id: '50bba6a865b54bbf8a1a4e27d2c4cfac',
+  },
+  'menopay': {
+    id: 'd74587a5604f43cab0377ede534062fb',
+  },
+};
--- src/pages/case_studies/[slug].tsx
+++ src/pages/case_studies/[slug].tsx
@@ -1,49 +1,43 @@
-import { PostContent } from "@/components/PageContent";
-import { getPostBlocks } from "@/lib/block";
-import { BlockWithChildren } from "@/lib/blockType";
-import { getPosts } from "@/lib/posts";
-import { IPost } from "@/lib/postsType";
-import { GetStaticProps } from "next";
+import { getPosts } from "@/lib/caseStudy";
+import { MdStringObject } from "notion-to-md/build/types";
+import { NotionToMarkdown } from "notion-to-md";
+import { CaseStudyPosts, notion } from "@/lib/notion";
 
 export async function getStaticPaths() {
   const posts = await getPosts();
-  console.log(posts);
-  console.log(posts.map((post) => post.path));
+  // console.log(posts);
+  // console.log(posts.map((post) => post.path));
   return {
     paths: [],
     fallback: true,
   };
 }
+const n2m = new NotionToMarkdown({ notionClient: notion });
 
-export const getStaticProps: GetStaticProps = async () => {
-  const posts = await getPosts();
-  const AllBlocks: BlockWithChildren[][] = (
-    await Promise.all(
-      posts.map((post) => {
-        return getPostBlocks(post.id);
-      })
-    )
-  );
-  return {
-    props: {
-      posts,
-      AllBlocks,
-    },
+type Params = {
+  params: {
+    slug: keyof typeof CaseStudyPosts;
   };
 };
 
-interface Props {
-  posts: IPost[];
-  AllBlocks: BlockWithChildren[][];
-}
-const NotionDomainDynamicPage = (props: Props) => {
-  return (
-    <>
-     {props.AllBlocks && props.AllBlocks.map((blocks, index) => {
-       return <PostContent key={index} blocks={blocks} />
-     })}
-    </>
-  );
+export const getStaticProps = async ({ params: { slug } }: Params) => {
+  const { id } = CaseStudyPosts[slug];
+  const mdblocks = await n2m.pageToMarkdown(id);
+  console.log(mdblocks);
+  const mdString = n2m.toMarkdownString(mdblocks);
+  console.log(mdString.parent);
+  return {
+    props: {
+      mdString,
+    },
+    revalidate: 10,
+  };
+};
+
+const NotionDomainDynamicPage = (props: { mdString: MdStringObject }) => {
+  console.log(props.mdString);
+  console.log(Object.values(props.mdString));
+  return <>{Object.values(props.mdString)}</>;
 };
 
 export default NotionDomainDynamicPage;
