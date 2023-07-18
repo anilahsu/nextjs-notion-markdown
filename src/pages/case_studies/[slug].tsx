4b62104432cc111ccb97147cae1f65ed510135ef
simple1017 <simple01281017@gmail.com>
2023-07-18 22:42:08 +0800

    feat: use the react-markdown tranform the markdown data

--- src/pages/case_studies/[slug].tsx
+++ src/pages/case_studies/[slug].tsx
@@ -2,6 +2,7 @@ import { getPosts } from "@/lib/caseStudy";
 import { MdStringObject } from "notion-to-md/build/types";
 import { NotionToMarkdown } from "notion-to-md";
 import { CaseStudyPosts, notion } from "@/lib/notion";
+import ReactMarkdown from "react-markdown";
 
 export async function getStaticPaths() {
   const posts = await getPosts();
@@ -25,19 +26,35 @@ export const getStaticProps = async ({ params: { slug } }: Params) => {
   const mdblocks = await n2m.pageToMarkdown(id);
   console.log(mdblocks);
   const mdString = n2m.toMarkdownString(mdblocks);
-  console.log(mdString.parent);
+  const markdown = mdString.parent;
+
   return {
     props: {
       mdString,
+      markdown,
     },
     revalidate: 10,
   };
 };
 
-const NotionDomainDynamicPage = (props: { mdString: MdStringObject }) => {
-  console.log(props.mdString);
-  console.log(Object.values(props.mdString));
-  return <>{Object.values(props.mdString)}</>;
+const NotionDomainDynamicPage = (props: {
+  mdString: MdStringObject;
+  markdown: string;
+}) => {
+  if (props.mdString) {
+    console.log(props.mdString);
+    console.log(Object.values(props.mdString));
+  }
+  return (
+    <ReactMarkdown
+      remarkPlugins={[]}
+      components={{
+
+      }}
+    >
+      {props.markdown}
+    </ReactMarkdown>
+  );
 };
 
 export default NotionDomainDynamicPage;
