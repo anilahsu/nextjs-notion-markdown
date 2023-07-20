10a90606b9282831eb213311e184775d3eb73850
simple1017 <simple01281017@gmail.com>
2023-07-20 20:13:35 +0800

    feat: update the nextjs config for remote url images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pay.pocket-change.jp',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pokepay-assets.s3.ap-northeast-1.amazonaws.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
