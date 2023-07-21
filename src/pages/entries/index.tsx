851186daa25e53559b97eafd070b8d0bdc9007ea
simple1017 <simple01281017@gmail.com>
2023-07-21 10:56:10 +0800

    feat: add entry post list page

---
+++ src/pages/entries/index.tsx
@@ -0,0 +1,86 @@
+import { GetStaticProps } from "next";
+import { IPost } from "@/lib/entryType";
+import { getEntryPosts } from "@/lib/entry";
+import Link from "next/link";
+import styled from "@emotion/styled";
+import Image from "next/image";
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getEntryPosts();
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+interface Props {
+  posts: IPost[];
+}
+
+const EntryList = ({ posts }: Props) => {
+  console.log(posts)
+  return (
+    <Container>
+      {posts.map((post) => {
+        return (
+          <Link key={post.path} href={"/entries/" + post.path}>
+            <Card>
+              {post.cover && (
+                <CoverImage
+                  src={post.cover}
+                  alt={""}
+                  width={700}
+                  height={300}
+                />
+              )}
+              <Title>{post.title}</Title>
+              <p>{post.description}</p>
+              <p>{post.modifiedDate}</p>
+              <p>{post.url}</p>
+              <p>{post.path}</p>
+              <p>{post.id}</p>
+
+              <span>
+                {post.tags.map((tag, index) => (
+                  <span key={index}>{tag}</span>
+                ))}
+              </span>
+            </Card>
+          </Link>
+        );
+      })}
+    </Container>
+  );
+};
+
+export default EntryList;
+
+const Container = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const Card = styled.div`
+  padding: 20px;
+  border: 1px solid #2d2d2d;
+  margin: 10px;
+  border-radius: 20px;
+  box-shadow: 0 0 15px 10px rgba(0, 0, 0, 0.05);
+  display: flex;
+  max-width: 700px;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const CoverImage = styled(Image)`
+  width: 100%;
+  height: auto;
+`;
+const Title = styled.h2`
+  font-size: 20px;
+  line-height: 30px;
+  text-align: center;
+`;
