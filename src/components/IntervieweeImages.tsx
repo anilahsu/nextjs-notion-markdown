1b1dcfcd8decd424685cf7e14ce23b2d3107a83f
simple1017 <simple01281017@gmail.com>
2023-08-01 11:11:41 +0800

    feat: add ServiceIntroduction and InterviewImages components

---
+++ src/components/IntervieweeImages.tsx
@@ -0,0 +1,11 @@
+import { IPost } from "@/lib/caseStudyType";
+
+const IntervieweeImages = ({ context }: { context: IPost }) => {
+  return (
+    <>
+      <p>IntervieweeImages</p>
+    </>
+  );
+};
+
+export default IntervieweeImages;
---
+++ src/components/ServiceIntroduction.tsx
@@ -0,0 +1,11 @@
+import { IPost } from "@/lib/caseStudyType";
+
+const ServiceIntroduction = ({ context }: { context: IPost }) => {
+  return (
+    <>
+      <p>ServiceIntroduction</p>
+    </>
+  );
+};
+
+export default ServiceIntroduction;
