1f67718559dcb2418f6918264221f15a16a74999
simple1017 <simple01281017@gmail.com>
2023-08-25 09:38:08 +0800

    feat: extract filterOrder for Posts

---
+++ src/utils/filterOrder.ts
@@ -0,0 +1,24 @@
+import { IPost } from "@/lib/caseStudyType";
+
+export const FilterOrder = (posts: IPost[]) => {
+  const publishedPosts = posts.filter((post) => post.published);
+
+  publishedPosts.sort((a, b) => {
+    // sort by priority in ascending order
+    if (a.priority && b.priority && a.priority > b.priority) {
+      return 1;
+    }
+    if (a.priority && b.priority && a.priority < b.priority) {
+      return -1;
+    }
+    // If priority is equal, sort by modifiedDate in descending order
+    if (new Date(a.modifiedDate) > new Date(b.modifiedDate)) {
+      return -1;
+    }
+    if (new Date(a.modifiedDate) < new Date(b.modifiedDate)) {
+      return 1;
+    }
+    return 0; // Objects are equal
+  });
+  return publishedPosts;
+};
